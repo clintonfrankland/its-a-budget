4445c6d259be9e3a2d55ec24bee602d5c9a690b6
clintonfrankland <clintonfrankland@users.noreply.github.com>
2025-10-18 11:14:07 -0400

    Fix login page markup and password decryption

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -137,11 +137,9 @@ namespace ClintonFrankland
                 var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6E, 0x20, 0x4D, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
                 encryptor.Key = pdb.GetBytes(32);
                 encryptor.IV = pdb.GetBytes(16);
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
                 using (var ms = new MemoryStream())
                 {
-                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
                     {
                         cs.Write(cipherBytes, 0, cipherBytes.Length);
                         cs.Close();
