f51c4f34a83c4d628ae2641559c9e234ef3afcea
Clinton Frankland <clinton@debopenclawvm.home>
2026-03-04 13:38:44 -0500

    Budget App: remove legacy WebForms; move Blazor app to repo root

--- ClintonFrankland.Blazor/Models/PageInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace ClintonFrankland.Models;
-
-public class PageInfo
-{
-    public int PageId { get; set; } = -1;
-    public string PageName { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-}
